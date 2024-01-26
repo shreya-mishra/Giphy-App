04ef187d4950fe3ac32cab23bcbb66ae974349a1
shreya-mishra <shreyamishra040398@gmail.com>
2024-01-26 19:36:15 +0530

    added handle search input function in paresnt file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,61 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 import {getGiphyData} from './GetData/GetGiphyData';
+import DisplayGifs from './components/DisplayGifs';
+import SearchInput from './components/SearchInput';
 
 const App = () => {
+  const [giphyData, setGiphyData] = useState([]);
+  const [searchVal, setSearchVal] = useState<string>();
+  const [searchGif, setSearchGif] = useState<string>();
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [page,setPage] = useState(1)
+
   useEffect(() => {
     fetchGifData();
-  });
+  }, [giphyData]);
 
+  const handleSearch = () => {
+    setSearchGif(searchVal);
+  };
   const fetchGifData = async () => {
-    await getGiphyData();
+    try {
+      setIsLoading(true);
+      const res = await getGiphyData();
+      if (searchVal?.length > 0 || searchGif?.length > 0) {
+        const filteredData = giphyData.filter(item =>
+          item.username.toLowerCase().includes(searchVal.toLowerCase()),
+        );
+        setSearchResults(filteredData);
+        setGiphyData(filteredData);
+        setSearchVal('');
+      }
+      setGiphyData(res);
+    } catch (error) {
+      console.error('error fetching data', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const handleEndReached = () => {
+    if (!isLoading) {
+      fetchGifData();
+    }
   };
   return (
     <View>
-      <Text>Hello Giphy</Text>
+      <SearchInput
+        setSearchVal={setSearchVal}
+        searchVal={searchVal}
+        handleSearch={handleSearch}
+      />
+      <DisplayGifs
+        giphyData={giphyData}
+        searchGif={searchVal}
+        isLoading={isLoading}
+        handleEndReached={handleEndReached}
+      />
     </View>
   );
 };
